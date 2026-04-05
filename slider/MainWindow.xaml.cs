86bba77a43ff48ead3cac7b75190a2db4fb42673
Nikos <Nikos@PC-IT2>
2026-04-05 02:28:32 +0200

    Добавьте файлы проекта.

---
+++ slider/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace slider
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenSlider_Click(object sender, RoutedEventArgs e)
+        {
+            SliderWindow slider = new SliderWindow();
+            slider.Show();
+        }
+    }
+}
---
+++ slider/SliderWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace slider
+{
+    public partial class SliderWindow : Window
+    {
+        private List<string> images = new List<string>()
+        {
+            @"C:\Temp\1.jpg",
+            @"C:\Temp\2.jpg",
+            @"C:\Temp\3.jpg"
+        };
+
+        private int currentIndex = 0;
+        private DispatcherTimer timer;
+
+        public SliderWindow()
+        {
+            InitializeComponent();
+
+            ShowImage();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            currentIndex++;
+
+            if (currentIndex >= images.Count)
+                currentIndex = 0;
+
+            ShowImage();
+        }
+
+        private void ShowImage()
+        {
+            if (images.Count == 0)
+                return;
+
+            SlideImage.Source = new BitmapImage(new Uri(images[currentIndex]));
+        }
+    }
+}
