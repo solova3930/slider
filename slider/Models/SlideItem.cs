d7fa419c6012bc3d05a0c5e2f4083c70d1a77f2c
Nikos <Nikos@PC-IT2>
2026-04-05 06:08:13 +0200

    asdasd

---
+++ slider/Models/SlideItem.cs
@@ -0,0 +1,10 @@
+﻿namespace slider.Models
+{
+    public class SlideItem
+    {
+        public string FileName { get; set; } = "";
+        public string ImagePath { get; set; } = "";
+        public int DurationSeconds { get; set; } = 5;
+        public string TransitionEffect { get; set; } = "Затухание";
+    }
+}
---
+++ slider/Services/PlaylistFileService.cs
@@ -0,0 +1,28 @@
+﻿using slider.Models;
+using System.IO;
+using System.Text.Json;
+
+namespace slider.Services
+{
+    public static class PlaylistFileService
+    {
+        private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        public static void Save(string filePath, PlaylistData playlist)
+        {
+            string json = JsonSerializer.Serialize(playlist, JsonOptions);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static PlaylistData Load(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            var playlist = JsonSerializer.Deserialize<PlaylistData>(json, JsonOptions);
+
+            return playlist ?? new PlaylistData();
+        }
+    }
+}
